7e16e573f4b8a72e5b05c1e9cdf6d3703bff48f3
Chanwoong(Noah) Park <135056537+Noah1215@users.noreply.github.com>
2023-08-13 22:54:10 -0400

    modify-paragraph

--- src/type.js
+++ src/type.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-new TypeIt('.home__title--strong', {
+new TypeIt(".home__title--strong", {
   loop: true,
   speed: 100,
 })
-  .move(-11)
-  .type('Amazing ')
+  .move(-13)
+  .type("Passionate ")
   .pause(2000)
-  .move(null, {to: 'END'})
+  .move(null, { to: "END" })
   .delete()
   .go();
